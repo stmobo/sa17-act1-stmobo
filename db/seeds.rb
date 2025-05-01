de4a93f68f217a408e510c0757624a4d0f1d1753
Sebastian Mobo <stmobo@gmail.com>
2025-04-30 20:24:40 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+BoardGame.create!(
+  title: "Scrabble",
+  category: "Word Game",
+  min_players: 2,
+  max_players: 4,
+  publisher: "Hasbro",
+  description: "Place tiles containing letters to form words and score points."
+)
+
+BoardGame.create!(
+  title: "Risk",
+  category: "Strategy",
+  min_players: 2,
+  max_players: 6,
+  publisher: "Hasbro",
+  description: "Command an army and conquer the world by rolling dice."
+)
+
+BoardGame.create!(
+  title: "Clue",
+  category: "Deduction",
+  min_players: 3,
+  max_players: 6,
+  publisher: "Hasbro",
+  description: "Assemble clues and solve a murder mystery."
+)
+
+BoardGame.create!(
+  title: "Diplomacy",
+  category: "Strategy",
+  min_players: 2,
+  max_players: 7,
+  publisher: "Wizards of the Coast",
+  description: "Command an army and conquer Europe by backstabbing your 'friends'."
+)
